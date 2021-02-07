56ebd0bdf8ee6ab645abd9f1a4e1fda8d3419a65
Vyoma <vyoma.shah92@gmail.com>
2021-02-07 01:24:44 -0500

    Update function.php
    Update function.php file as per requirement and edit related files. Remove extra code and files

--- themes/sport/functions.php
+++ themes/sport/functions.php
@@ -50,7 +50,7 @@ if ( ! function_exists( 'sport_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary', 'sport' ),
+				'menu-primary' => esc_html__( 'Primary', 'sport' ),
 			)
 		);
 
@@ -68,18 +68,6 @@ if ( ! function_exists( 'sport_setup' ) ) :
 				'caption',
 				'style',
 				'script',
-			)
-		);
-
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'sport_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
 			)
 		);
 
@@ -125,7 +113,7 @@ function sport_widgets_init() {
 	register_sidebar(
 		array(
 			'name'          => esc_html__( 'Sidebar', 'sport' ),
-			'id'            => 'sidebar-1',
+			'id'            => 'sidebar',
 			'description'   => esc_html__( 'Add widgets here.', 'sport' ),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
@@ -141,20 +129,12 @@ add_action( 'widgets_init', 'sport_widgets_init' );
  */
 function sport_scripts() {
 	wp_enqueue_style( 'sport-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'sport-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'sport-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'sport_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -170,11 +150,3 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
