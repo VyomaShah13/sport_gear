fe43df556499a7ce2198e5413c2728eb3a3e6f6e
Vyoma <vyoma.shah92@gmail.com>
2021-02-08 05:24:10 -0500

    Integrate bootstrap framework
    Add bootstrap files into theme

--- themes/sport/functions.php
+++ themes/sport/functions.php
@@ -208,6 +208,18 @@ add_action( 'widgets_init', 'sport_widgets_init' );
  */
 function sport_scripts() {
 	wp_enqueue_style( 'sport-style', get_stylesheet_uri(), array(), _S_VERSION );
+
+	/**
+	 * Bootstrap Framework assets
+	 */
+	wp_enqueue_style( 
+		'bootstrap-style', 
+		get_template_directory_uri() . '/assets/css/vendor/bootstrap.min.css' 
+	);
+	wp_enqueue_script( 
+		'bootstrap-script', 
+		get_template_directory_uri() . '/assets/js/vendor/bootstrap.min.js' , array(), false, true
+	);
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
