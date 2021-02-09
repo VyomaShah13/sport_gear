e687c738cca28a6079613a13c7575939c63bba0c
Vyoma <vyoma.shah92@gmail.com>
2021-02-09 12:01:14 -0500

    Footer design

--- themes/sport/footer.php
+++ themes/sport/footer.php
@@ -10,21 +10,45 @@
  */
 
 ?>
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sport' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sport' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sport' ), 'sport', '<a href="http://vyomashah.com/">Vyoma Shah</a>' );
-				?>
-		</div><!-- .site-info -->
+	<div class="newsletter">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="newsletter-text">
+						<h4>Newsletter</h4>
+						<p>Subscribe our newsletter and  keep update fo new product & Promotion</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<form action="post">
+						<div class="newsletter-form">
+							<input id="newsletter-email" class="input-control has-white-background-color" type="email" placeholder="Your Email Address" required="required" >
+							<button id="newsletter-submit" type="submit" class="primary-btn has-primary-background-color has-grayish-black-color" value="Submit">subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer id="colophon" class="site-footer has-secondary-background-color has-white-color has-regular-font-size">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="site-info">
+						© 2021 <a href="#">SportGear</a>. All Rights Reserved. 
+					</div><!-- .site-info -->
+				</div>
+				<div class="col-lg-6">
+					<div class="social-links">
+						<ul class="text-right">
+							<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+						</ul>
+					</div><!-- .site-info -->
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
