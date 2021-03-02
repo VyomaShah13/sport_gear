c1c55ca657d936c1e2fe77bd6a1879bed04f4e6a
Vyoma <vyoma.shah92@gmail.com>
2021-03-02 08:39:39 -0500

    Add container class to product page

--- themes/sport/inc/woocommerce.php
+++ themes/sport/inc/woocommerce.php
@@ -20,13 +20,4 @@
         return $fragments;
     }
     add_filter( 'woocommerce_add_to_cart_fragments', 'sport_woocommerce_add_to_cart_fragments' );
-    add_filter( 'single-product', 'custom_woocommerce_product_loop_title_classes' );
-    /**
-     * Append custom class(es) to the default WooCommerce product title class.
-     *
-     * @param string $class Existing class(es).
-     * @return string Modified class(es).
-     */
-    function custom_woocommerce_product_loop_title_classes( $class ) {
-        return $class . ' new-class and-another'; // set your additional class(es) here.
-    }
+    
--- themes/sport/template-parts/content.php
+++ themes/sport/template-parts/content.php
@@ -34,29 +34,31 @@
 	<?php sport_post_thumbnail(); ?>
 	
 	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'sport' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+		<div class="container">
+			<?php
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'sport' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sport' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sport' ),
+					'after'  => '</div>',
+				)
+			);
+			?>
+		</div>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
