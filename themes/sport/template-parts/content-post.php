ed277fc62b4c16ee17b3bfd17a24100fbdb5c208
Vyoma <vyoma.shah92@gmail.com>
2021-03-09 15:19:18 -0500

    Set blog listing page
    Display some details about each page

--- themes/sport/template-parts/content-post.php
+++ themes/sport/template-parts/content-post.php
@@ -35,7 +35,20 @@
 	<div class="entry-content">
 		<div class="container">
 			<?php
-			the_excerpt();
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'sport' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
 			
 
 			wp_link_pages(
